04172f046cca500e21156185f2a150bc780f77b7
betodt <alberto.diaz.tostado@gmail.com>
2016-01-17 18:58:07 -0800

    removed ic ajax

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -29,5 +29,9 @@ module.exports = function(defaults) {
   app.import('bower_components/picnic/releases/plugins.min.css');
   app.import('bower_components/picnic/releases/picnic.min.css');
 
+  app.import('bower_components/moment/moment.js');
+
+  app.import('bower_components/ic-ajax/dist/named-amd/main.js');
+
   return app.toTree();
 };
